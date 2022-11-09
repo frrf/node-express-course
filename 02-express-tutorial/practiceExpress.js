924fe8c504ebc15808ca35b8c4100a8cdf5dc905
frrf <franciscopali71@gmail.com>
2022-11-08 23:19:20 -0800

    week 4

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const middleware = require('./practice-middleware.js')
+
 
 app.use(express.static('./new-public'))
+app.use(middleware)
 
 app.get('/sample', (req, res) => {
   res.send('<h2>This is Working</h2>')

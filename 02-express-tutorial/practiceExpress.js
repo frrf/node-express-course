6d5501ee2908c0a624d16a83df6bb9291575c018
frrf <franciscopali71@gmail.com>
2022-11-14 19:01:32 -0800

    extra middleware inspecting header's content-type

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const middleware = require('./practice-middleware.js')
-
+const extraMiddleware = require('./extraChallengeMiddleware.js')
 
 app.use(express.static('./new-public'))
-app.use(middleware)
+app.use(middleware,extraMiddleware)
 
 app.get('/sample', (req, res) => {
   res.send('<h2>This is Working</h2>')
 })
+
 
 
 app.listen(3000, ()=> {
